d03f5f02a61c966a29009a21195d23027d1a4e63
realjanpaulus <janpaulus95@web.de>
2020-03-11 13:20:54 +0100

    add: prose to csv

--- texts_to_csv.py
+++ texts_to_csv.py
@@ -1,14 +1,28 @@
 import argparse
+import logging
 from nltk import word_tokenize
 import numpy as np
 import os
 import pandas as pd
 from pathlib import Path
+import shutil
 import re
 from typing import Dict, List, Optional, Tuple
 import urllib
 import xmltodict
 import zipfile
+
+### texts_to_csv logging handler ###
+
+# TODO: überprüfen, es soll nrue einmal logging kommen
+
+logging.basicConfig(level=logging.INFO, filename="texts_to_csv.log", filemode="w")
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s",
+                              datefmt='%H:%M')
+console.setFormatter(formatter)
+logging.getLogger("").addHandler(console)
 
 
 def get_file_list(path: str) -> List[str]:
@@ -80,8 +94,58 @@ def get_metadata(filename: str, file: str) -> List:
     text = " ".join(tok_text)
     return metalist + [textlength, text]
 
+def prose_to_df(path: str,
+                used_dataset: Optional[str] = "corpus-of-german-fiction-txt",
+                zip_file: Optional[str] = "CorpusofGermanLanguageFiction.zip",
+                keep_unzipped: Optional[bool] = False) -> pd.DataFrame:
+    """ Takes a path to a directory of text files and saves
+        the metadata and the corresponding text into a
+        DataFrame.
+    """
+
+    #TODO: logging und df weg
+    logging.info(f"Extracting zipfile.")
+    df = None
+
+    zip_path = os.path.join(path, zip_file)
+
+    tmp = {}
+
+    with zipfile.ZipFile(zip_path) as file:
+        file.extractall(path=path)
+
+    
+    dataset_path = os.path.join(path, f"Corpus of German-Language Fiction/{used_dataset}")
+    file_list = get_file_list(dataset_path) 
+    
+    
+    logging.info(f"Parsing texts from '{used_dataset}'.")
+
+    # pare file_list and add meta data with text to dict
+    for c, filename in enumerate(file_list):
+
+        file_dir = dataset_path + "/" + filename + ".txt"
+        with open(file_dir, encoding="utf-8") as f:
+            tmp_file = f.read()
+            tmp[filename] = get_metadata(filename, tmp_file)
+
+    logging.info("Creating the DataFrame.")
+    # dict to dataframe
+    df = pd.DataFrame.from_dict(tmp, orient="index").reset_index()
+    df.columns = ["filename", "author", "title", 
+                  "year", "textlength", "text"]
+    
+    if not keep_unzipped:
+        logging.info(f"Unzipped files will be deleted. `keep_unzipped = True` prevents this.")
+        shutil.rmtree('Corpus of German-Language Fiction', ignore_errors=True)
+    
+    return df
+
+
+
 def speeches_to_df(path: str, 
-                   remote_dataset: Optional[str] = "Bundesregierung.xml") -> pd.DataFrame:
+                   used_dataset: Optional[str] = "Bundesregierung.xml",
+                   zip_file: Optional[str] = "German-Political-Speeches-Corpus.zip") -> pd.DataFrame:
     """ Takes a path to the Zipfile 'German-Political-Speeches-Corpus.zip' and 
         returns a DataFrame. Only the XML-file 'Bundesregierung.xml' will be used.
 
@@ -89,55 +153,37 @@ def speeches_to_df(path: str,
         INFO: The code was adopted in a modified form 
         from the following website: https://www.timmer-net.de/2019/03/24/nlp_basics/
     """
+    logging.info(f"Extracting speeches from '{used_dataset}'.")
     df = None
-
-    zip_file = "German-Political-Speeches-Corpus.zip"
 
     zip_path = os.path.join(path, zip_file)
 
     with zipfile.ZipFile(zip_path) as file:
-        file.extract(remote_dataset, path=path)
+        file.extractall(used_dataset, path=path)
         
-    xml_path = os.path.join(path, remote_dataset)
+    xml_path = os.path.join(path, used_dataset)
     with open(xml_path, mode="rb") as file:
         xml_document = xmltodict.parse(file)
         nodes = xml_document['collection']['text']
-        df = pd.DataFrame({'author' : [t['@person'] for t in nodes],
-                            'title' : [t['@titel'] for t in nodes],
-                            'text' : [t['rohtext'] for t in nodes]})
+        df = pd.DataFrame({'speaker' : [t['@person'] for t in nodes],
+                           'title' : [t['@titel'] for t in nodes],
+                           'speech' : [t['rohtext'] for t in nodes]})
 
     return df
 
-def texts_to_df(path: str) -> pd.DataFrame:
-    """ Takes a path to a directory of text files and saves
-        the metadata and the corresponding text into a
-        DataFrame.
-    """
-    d = {}
-    file_list = get_file_list(path)
 
-    # pare file_list and add meta data with text to dict
-    for c, filename in enumerate(file_list):
-
-        file_dir = path + "/" + filename + ".txt"
-        with open(file_dir, encoding="utf-8") as f:
-            tmp_file = f.read()
-            d[filename] = get_metadata(filename, tmp_file)
-
-    # dict to dataframe
-    df = pd.DataFrame.from_dict(d, orient="index").reset_index()
-    df.columns = ["filename", "author", "title", 
-                  "year", "textlength", "text"]
-    return df
 
 def main():
+
     # TODO: evtl. mehr corpora hinzufügen
     if args.corpus_name == "prose":
-        df = texts_to_df(args.path)
-        df.to_csv("../corpora/corpus.csv", index=False)
+        df = prose_to_df(args.path)
+        logging.info(f"Writing csv-file to corpora/")
+        df.to_csv("corpora/german_prose.csv", index=False)
     elif args.corpus_name == "speeches":
         df = speeches_to_df(args.path)
-        df.to_csv("../corpora/speeches_corpus.csv", index=False)
+        logging.info(f"Writing csv-file to corpora/")
+        df.to_csv("corpora/german_speeches.csv", index=False)
 
 if __name__ == "__main__":
     
